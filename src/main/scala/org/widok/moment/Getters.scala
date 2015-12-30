65228038065e2eee207ec0e0972e3244ec814acb
Didier Mounoud <dmounoud@diadys.ch>
2015-12-30 21:23:35 +0100

    add date and weekday getters

--- src/main/scala/org/widok/moment/Getters.scala
+++ src/main/scala/org/widok/moment/Getters.scala
@@ -8,6 +8,8 @@ trait Getters extends js.Object {
   def second(): Double = js.native
   def minute(): Double = js.native
   def hour(): Double = js.native
+  def date(): Double = js.native
+  def weekday(): Double = js.native
   def day(): Double = js.native
   def month(): Double = js.native
   def year(): Double = js.native
